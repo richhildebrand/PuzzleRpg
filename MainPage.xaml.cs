10762af62e0dd9d1e0162aa945278a13965efece
Rich Hildebrand <richhildebrand@gmail.com>
2013-12-28 17:21:47 -0500

    adjust z on drag

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
@@ -13,29 +14,24 @@ namespace PuzzleRpg
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
+
         public MainPage()
         {
             InitializeComponent();
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        private void MouseMoving(object sender, MouseEventArgs e)
+        {
+            var image = (System.Windows.Controls.Image)sender;
+            Canvas.SetZIndex(image, 1);
+            var x = 7;
+        }
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        public void OnDragEnd(object sender, ManipulationCompletedEventArgs e)
+        {
+            var image = (System.Windows.Controls.Image)sender;
+            Canvas.SetZIndex(image, 0);
+            var x = 8;
+        }
     }
 }
