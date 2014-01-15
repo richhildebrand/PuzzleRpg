763555d6fa8ac77d3f3dec1fe504488711281729
Rich Hildebrand <richhildebrand@gmail.com>
2014-01-14 19:21:51 -0500

    create hero profile control

---
+++ CustomControls/HeroProfile.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using PuzzleRpg.Heroes;
+using PuzzleRpg.Utils;
+
+namespace PuzzleRpg.CustomControls
+{
+    public partial class HeroProfile : UserControl
+    {
+        public Hero ThisHero { get; set; }
+
+        public HeroProfile(Hero hero)
+        {
+            ThisHero = hero;
+            InitializeComponent();
+            DrawProfile(ThisHero);
+        }
+
+        private void DrawProfile(Hero hero)
+        {
+            ProfilePicture.ImageSource = ImageUtils.GetImageSourceFromPath("/" + hero.ProfileImagePath);
+        }
+    }
+}
--- Heroes/HeroGrid.cs
+++ Heroes/HeroGrid.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Media;
+using PuzzleRpg.CustomControls;
 using PuzzleRpg.Utils;
 
 namespace PuzzleRpg.Heroes
@@ -32,21 +30,18 @@ namespace PuzzleRpg.Heroes
         {
             if (hero != null)
             {
-                var heroIcon = ImageUtils.GetImageFromPath(hero.ProfileImagePath);
-                heroIcon.Tag = hero;
-                grid.Children.Add(heroIcon);
-                heroIcon.SetValue(Grid.ColumnProperty, column);
-                heroIcon.SetValue(Grid.RowProperty, 0);
-                heroIcon.Stretch = System.Windows.Media.Stretch.Fill;
-                heroIcon.Tap += OnSelectHero;
+                var heroProfile = new HeroProfile(hero);
+                grid.Children.Add(heroProfile);
+                heroProfile.SetValue(Grid.RowProperty, 0);
+                heroProfile.SetValue(Grid.ColumnProperty, column);
+                heroProfile.Tap += OnSelectHero;
             }
         }
   
         private void OnSelectHero(object sender, GestureEventArgs e)
         {
-            var clickedIcon = sender as Image;
-            var selectedHero = clickedIcon.Tag as Hero;
-            var castSpellModal = new CastSpellModal(selectedHero);
+            var selectedHeroProfile = sender as HeroProfile;
+            var castSpellModal = new CastSpellModal(selectedHeroProfile.ThisHero);
             castSpellModal.Show();
         }
     }
