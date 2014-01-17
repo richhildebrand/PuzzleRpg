56ed471b2643e82a37d6156845a75c3f9f42ba29
Rich Hildebrand <richhildebrand@gmail.com>
2014-01-16 21:46:28 -0500

    less clever heroRepository

--- HeroBox.xaml.cs
+++ HeroBox.xaml.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using Microsoft.Phone.Controls;
 using PuzzleRpg.CustomControls;
 using PuzzleRpg.Database;
+using PuzzleRpg.Utils;
 
 namespace PuzzleRpg
 {
     public partial class HeroBox : PhoneApplicationPage
     {
+        private readonly int HEROES_PER_ROW = 5;
+
         public HeroBox()
         {
             InitializeComponent();
 
             // TODO: move somewhere that makes more sense
-            if (HeroRepository.GetPlayerHeroes().Count == 0)
-            {
+            //if (HeroRepository.GetPlayerHeroes().Count == 0)
+            //{
                 HeroRepository.AddPlayerHero(HeroDatabase.GetHero(0));
                 HeroRepository.AddPlayerHero(HeroDatabase.GetHero(1));
                 HeroRepository.AddPlayerHero(HeroDatabase.GetHero(2));
@@ -24,7 +28,7 @@ namespace PuzzleRpg
                 HeroRepository.AddPlayerHero(HeroDatabase.GetHero(5));
                 HeroRepository.AddPlayerHero(HeroDatabase.GetHero(6));
                 HeroRepository.AddPlayerHero(HeroDatabase.GetHero(7));
-            }
+            //}
 
             LoadPlayerHeroes(HeroGrid);
         }
@@ -32,13 +36,20 @@ namespace PuzzleRpg
         private void LoadPlayerHeroes(Grid heroGrid)
         {
             var playerHeroes = HeroRepository.GetPlayerHeroes();
-            foreach (var hero in playerHeroes)
+            var numberOfRowsToDisplay = playerHeroes.Count / HEROES_PER_ROW;
+
+            GridUtils.AddColumnsToGrid(heroGrid, HEROES_PER_ROW);
+            GridUtils.AddRowsToGrid(heroGrid, numberOfRowsToDisplay);
+
+            for (int i = 0; i < playerHeroes.Count; i++)
             {
                 var heroProfile = new HeroProfile();
-                heroProfile.DrawHeroProfile(hero);
+                heroProfile.DrawHeroProfile(playerHeroes[i]);
                 heroGrid.Children.Add(heroProfile);
+                heroProfile.SetValue(Grid.RowProperty, i / HEROES_PER_ROW);
+                heroProfile.SetValue(Grid.ColumnProperty, i);
+                heroProfile.Height = Application.Current.Host.Content.ActualWidth / 5;
             }
-
         }
     }
 }
