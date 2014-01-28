0fd70ea5eab8b516e1d990451c255a2b178b795d
Rich Hildebrand <richhildebrand@gmail.com>
2014-01-27 20:27:28 -0500

    commented out hack - might want it later

--- Logic/MonsterGrid.cs
+++ Logic/MonsterGrid.cs
@@ -18,14 +18,14 @@ namespace PuzzleRpg.Logic
         public HealthBar MonsterHealth { get; set; }
         public Monster ActiveMonster { get; set; }
 
-        private int HACK_currentMonster = 0;//TODO: REMOVE THIS!!!
-        private void HACK_ToggleMonsterImage(object sender, GestureEventArgs e)
-        {
-            HACK_currentMonster += 1;
-            HACK_currentMonster = (HACK_currentMonster >= MonsterDatabase.MonsterCount()) ? 0 : HACK_currentMonster;
-            var monster = MonsterDatabase.GetMonster(HACK_currentMonster);
-            _monsterUI.MonsterImage.Source = ImageUtils.GetImageSourceFromPath("/" + monster.FullImagePath);
-        }
+        //private int HACK_currentMonster = 0;//TODO: REMOVE THIS!!!
+        //private void HACK_ToggleMonsterImage(object sender, GestureEventArgs e)
+        //{
+        //    HACK_currentMonster += 1;
+        //    HACK_currentMonster = (HACK_currentMonster >= MonsterDatabase.MonsterCount()) ? 0 : HACK_currentMonster;
+        //    var monster = MonsterDatabase.GetMonster(HACK_currentMonster);
+        //    _monsterUI.MonsterImage.Source = ImageUtils.GetImageSourceFromPath("/" + monster.FullImagePath);
+        //}
 
         public MonsterGrid(MonsterWithHealthBar monsterUI, Dungeon dungeon)
         {
@@ -56,7 +56,7 @@ namespace PuzzleRpg.Logic
         private void ActivateMonster(Monster monster)
         {
             _monsterUI.MonsterImage.Source = ImageUtils.GetImageSourceFromPath("/" + monster.FullImagePath);
-            _monsterUI.Tap += HACK_ToggleMonsterImage;
+            //_monsterUI.Tap += HACK_ToggleMonsterImage;
             ActiveMonster = monster;
             MonsterHealth.SetHealthPercentage(monster.CurrentHealth, monster.TotalHealth);
         }
