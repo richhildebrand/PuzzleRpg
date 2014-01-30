4ff40aa53ee4002683d2955ca11d4bf55483e6bd
Rich Hildebrand <richhildebrand@gmail.com>
2014-01-29 20:45:20 -0500

    give player starting heroes

--- Database/HeroRepository.cs
+++ Database/HeroRepository.cs
@@ -10,17 +10,17 @@ namespace PuzzleRpg.Database
     {
         private static readonly string HEROES_KEY = "Heroes";
 
-        public static List<Hero> GetPlayerHeroes()
+        public static List<Hero> GetHeroesOwnedByPlayer()
         {
             if (!IsolatedStorageSettings.ApplicationSettings.Contains(HEROES_KEY))
             {
-                return new List<Hero>();
+                GivePlayerStartingHeroes();
             }
             
             return IsolatedStorageSettings.ApplicationSettings[HEROES_KEY] as List<Hero>;
         }
 
-        public static void AddPlayerHero(Hero newHero)
+        public static void AddHeroToPlayerCollection(Hero newHero)
         {
             if (!IsolatedStorageSettings.ApplicationSettings.Contains(HEROES_KEY))
             {
@@ -30,11 +30,22 @@ namespace PuzzleRpg.Database
             }
             else
             {
-                var heroes = GetPlayerHeroes();
+                var heroes = GetHeroesOwnedByPlayer();
                 heroes.Add(newHero);
                 IsolatedStorageSettings.ApplicationSettings[HEROES_KEY] = heroes;
             }
             IsolatedStorageSettings.ApplicationSettings.Save();
+        }
+
+        private static void GivePlayerStartingHeroes() {
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(0));
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(1));
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(2));
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(3));
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(4));
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(5));
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(6));
+            AddHeroToPlayerCollection(HeroDatabase.GetHero(7));
         }
     }
 }
