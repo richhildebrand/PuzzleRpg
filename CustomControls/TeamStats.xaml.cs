bdfdf4dc07de8b4fbf9804d4c6155e4e55f9f5f0
Rich Hildebrand <richhildebrand@gmail.com>
2014-01-31 19:43:05 -0500

    init team summary design

---
+++ CustomControls/TeamStats.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace PuzzleRpg.CustomControls
+{
+    public partial class TeamStats : UserControl
+    {
+        public TeamStats()
+        {
+            InitializeComponent();
+        }
+    }
+}
