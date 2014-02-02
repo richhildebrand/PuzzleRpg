be0f53b5197025e15459d13d2c34c2da31f6a900
Rich Hildebrand <richhildebrand@gmail.com>
2014-02-01 23:09:22 -0500

    added team member class

---
+++ Models/TeamMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace PuzzleRpg.Models
+{
+    public class TeamMember
+    {
+        public int Slot { get; set; }
+        public Hero ThisHero { get; set; }
+
+        public TeamMember(int slot, Hero hero)
+        {
+            Slot = slot;
+            ThisHero = hero;
+        }
+    }
+}
