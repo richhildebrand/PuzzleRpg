5c68c175eedfbac79bcb7f729822aa682d473a28
Rich Hildebrand <richhildebrand@gmail.com>
2014-02-04 22:24:05 -0500

    added dungeons for easy monster access

--- Database/DungeonDatabase.cs
+++ Database/DungeonDatabase.cs
@@ -13,8 +13,43 @@ namespace PuzzleRpg.Database
         {
             AllDungeons = new List<Dungeon>();
 
-            AllDungeons.Add(GetGreenDragonsDen(1));
-            AllDungeons.Add(GetBlueDragonsDen(2));
+            AllDungeons.Add(GetRexsHouse(1));
+            AllDungeons.Add(GetOrcVilla(2));
+            AllDungeons.Add(GetTurtleShore(3));
+            AllDungeons.Add(GetGreenDragonsDen(4));
+            AllDungeons.Add(GetBlueDragonsDen(5));
+            AllDungeons.Add(GetDragonsDen(6));
+        }
+
+        private Dungeon GetTurtleShore(int id)
+        {
+            var dungeon = new Dungeon(id, "Turtle Shore");
+            dungeon.Floors.Add(new DungeonFloor(MonsterDatabase.GetMonster("Turtle")));
+            return dungeon;
+        }
+
+        private Dungeon GetRexsHouse(int id)
+        {
+            var dungeon = new Dungeon(id, "Rex's House");
+            dungeon.Floors.Add(new DungeonFloor(MonsterDatabase.GetMonster("Rex"))); 
+            return dungeon;
+        }
+
+        private Dungeon GetDragonsDen(int id)
+        {
+            var dungeon = new Dungeon(id, "Dragons Den");
+            dungeon.Floors.Add(new DungeonFloor(MonsterDatabase.GetMonster("GreenDragon")));
+            dungeon.Floors.Add(new DungeonFloor(MonsterDatabase.GetMonster("BlueDragon")));
+            dungeon.Floors.Add(new DungeonFloor(MonsterDatabase.GetMonster("RedDragon")));
+            return dungeon;
+        }
+
+        private Dungeon GetOrcVilla(int id)
+        {
+            var dungeon = new Dungeon(id, "Orc Villa");
+            dungeon.Floors.Add(new DungeonFloor(MonsterDatabase.GetMonster("WoodOrc")));
+            dungeon.Floors.Add(new DungeonFloor(MonsterDatabase.GetMonster("StoneOrc")));
+            return dungeon;
         }
 
         private Dungeon GetGreenDragonsDen(int id)
--- Logic/MonsterGrid.cs
+++ Logic/MonsterGrid.cs
@@ -17,15 +17,6 @@ namespace PuzzleRpg.Logic
         public HealthBar MonsterHealth { get; set; }
         public Monster ActiveMonster { get; set; }
 
-        //private int HACK_currentMonster = 0;//TODO: REMOVE THIS!!!
-        //private void HACK_ToggleMonsterImage(object sender, GestureEventArgs e)
-        //{
-        //    HACK_currentMonster += 1;
-        //    HACK_currentMonster = (HACK_currentMonster >= MonsterDatabase.MonsterCount()) ? 0 : HACK_currentMonster;
-        //    var monster = MonsterDatabase.GetMonster(HACK_currentMonster);
-        //    _monsterUI.MonsterImage.Source = ImageUtils.GetImageSourceFromPath("/" + monster.FullImagePath);
-        //}
-
         public MonsterGrid(MonsterWithHealthBar monsterUI, Dungeon dungeon)
         {
             _monsterUI = monsterUI;
@@ -55,7 +46,6 @@ namespace PuzzleRpg.Logic
         private Task ActivateMonster(Monster monster)
         {
             _monsterUI.MonsterImage.Source = ImageUtils.GetImageSourceFromPath("/" + monster.FullImagePath);
-            //_monsterUI.Tap += HACK_ToggleMonsterImage;
             ActiveMonster = monster;
             return MonsterHealth.SetHealthPercentage(monster.CurrentHealth, monster.TotalHealth);
         }
