aac1d3fbf6f92261e024f4318442909bd630e65b
Eric Fleming <eric-fleming18@hotmail.com>
2014-02-06 23:19:44 -0500

    removed unused using

--- CustomControls/NavigationBar.xaml.cs
+++ CustomControls/NavigationBar.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 using Microsoft.Phone.Controls;
 using SimpleMvvmToolkit;
 
