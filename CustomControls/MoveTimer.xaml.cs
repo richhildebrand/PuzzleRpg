2d7ee0893e933dd139fe121473a2b030ca9d3b41
Eric Fleming <eric-fleming18@hotmail.com>
2014-02-16 22:57:50 -0500

    Checkin for the night

--- CustomControls/MoveTimer.xaml.cs
+++ CustomControls/MoveTimer.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Windows;
+using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
+using PuzzleRpg.Utils;
 
 namespace PuzzleRpg.CustomControls
 {
@@ -15,6 +11,16 @@ namespace PuzzleRpg.CustomControls
         public MoveTimer()
         {
             InitializeComponent();
+        }
+
+        private Task DrawHealthBar(double newTime)
+        {
+            var timerColumn = Timer.RowDefinitions[0];
+            var oldMaxHeight = Convert.ToInt32(timerColumn.MaxHeight);
+            var newMaxHeight = Convert.ToInt32(newTime);
+
+            timerColumn.MaxHeight = newMaxHeight;
+            return Task.WhenAll(AnimateTimer.Animate(timerColumn, oldMaxHeight, newMaxHeight));
         }
     }
 }
---
+++ Utils/AnimateTimer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace PuzzleRpg.Utils
+{
+    public class AnimateTimer
+    {
+        private static TaskCompletionSource<bool> _taskSource;
+        private static Storyboard _healthStoryBoard;
+
+        public static Task Animate(RowDefinition row, int oldValue, int newValue)
+        {
+            _taskSource = new TaskCompletionSource<bool>();
+            _healthStoryBoard = new Storyboard();
+
+            if (oldValue != newValue)
+            {
+                DoAnimation(row, oldValue, newValue);
+            }
+            _healthStoryBoard.Completed += EndAnimation;
+            _healthStoryBoard.Begin();
+
+            return _taskSource.Task;
+        }
+
+        private static void EndAnimation(object sender, EventArgs e)
+        {
+            _healthStoryBoard.Stop();
+            _taskSource.SetResult(true);
+        }
+
+        private static void DoAnimation(RowDefinition row, double oldValue, double newValue)
+        {
+            Duration duration = new Duration(TimeSpan.FromMilliseconds(600));
+            CubicEase ease = new CubicEase { EasingMode = EasingMode.EaseOut };
+
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.EasingFunction = ease;
+            animation.Duration = duration;
+            _healthStoryBoard.Children.Add(animation);
+            animation.From = oldValue;
+            animation.To = newValue;
+            Storyboard.SetTarget(animation, row);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(ColumnDefinition.MaxWidth)"));
+        }
+    }
+}
