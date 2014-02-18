aa4d1ebf535f151cd15060a5d778402909be6699
Rich Hildebrand <richhildebrand@gmail.com>
2014-02-18 12:06:39 -0500

    init player options screen

---
+++ HeroOptions.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Phone.Controls;
+
+namespace PuzzleRpg
+{
+    public partial class HeroOptions : PhoneApplicationPage
+    {
+        public HeroOptions()
+        {
+            InitializeComponent();
+        }
+    }
+}
