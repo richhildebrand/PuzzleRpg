7ec3c8031bccaa2914577b8123986fb4627d3336
Rich Hildebrand <richhildebrand@gmail.com>
2014-02-19 21:23:30 -0500

    added nav to hero options

--- HeroOptions.xaml.cs
+++ HeroOptions.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.Phone.Controls;
+using SimpleMvvmToolkit;
 
 namespace PuzzleRpg
 {
@@ -9,6 +10,26 @@ namespace PuzzleRpg
         public HeroOptions()
         {
             InitializeComponent();
+        }
+
+        private void OnNavItemTapped(object sender, NotificationEventArgs e)
+        {
+            var url = e.Message;
+            NavigationService.Navigate(new Uri(url, UriKind.RelativeOrAbsolute));
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            NavBar.HighlightPage("/HeroOptions.xaml");
+            MessageBus.Default.Register("NavigateToPage", OnNavItemTapped);
+
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            MessageBus.Default.Unregister("NavigateToPage", OnNavItemTapped);
         }
     }
 }
