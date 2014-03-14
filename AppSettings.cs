2543d99a092a115ee6a475ee580eb30d8fe2d8fc
Rich Hildebrand <richhildebrand@gmail.com>
2014-03-14 17:43:37 -0400

    20 heroes for user

--- AppSettings.cs
+++ AppSettings.cs
@@ -8,7 +8,7 @@ namespace PuzzleRpg
         public static TimeSpan GainStaminaIntervalLength = TimeSpan.FromMinutes(30);
         public static int AmountOfStaminaToAddInterval = 3;
 
-        public static int MaxNumberOfHeroesPlayerCanOwn = 6;
+        public static int MaxNumberOfHeroesPlayerCanOwn = 20;
 
         public static int MonsterDeathFadeTimeInMilliseconds = 800;
         public static int MonsterDeathTimeInvisibleInMilliseconds = 200;
