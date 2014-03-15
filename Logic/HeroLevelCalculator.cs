ae1a29e797b6865c46a2015748fdbd153df6aabd
Rich Hildebrand <richhildebrand@gmail.com>
2014-03-15 11:43:27 -0400

    Can Get Experience Needed for a level

--- Logic/HeroLevelCalculator.cs
+++ Logic/HeroLevelCalculator.cs
@@ -5,10 +5,17 @@ namespace PuzzleRpg.Logic
 {
     public class HeroLevelCalculator
     {
-        public double GetLevelFrom(double baseExp, double currentExp) {
-            var levelComparedToBaseExp = currentExp / baseExp + 1;
+        public double GetLevelFrom(double baseExpPerLevel, double currentExp)
+        {
+            var levelComparedToBaseExp = currentExp / baseExpPerLevel + 1;
             var heroLevel = Math.Log(levelComparedToBaseExp, AppSettings.ExpericencePerLevelMultiplier);
             return Math.Floor(heroLevel) + 1;
+        }
+
+        public double GetExpNeededForLevel(int level, double baseExpPerLevel)
+        {   
+            var multiplier = Math.Pow(AppSettings.ExpericencePerLevelMultiplier, level - 1) -1;
+            return baseExpPerLevel * multiplier;
         }
     }
 }
