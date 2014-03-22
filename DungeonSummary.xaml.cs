62acf1a02d14e77d0d2d36d5a3dd1a32e1160af3
Rich Hildebrand <richhildebrand@gmail.com>
2014-03-22 10:24:07 -0400

    display all heroes in team on summary page

--- DungeonSummary.xaml.cs
+++ DungeonSummary.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Microsoft.Phone.Controls;
 using PuzzleRpg.Database;
@@ -25,15 +27,21 @@ namespace PuzzleRpg
                 dungeonDefeater.Defeat();
             }
 
-            GiveEarnedExpToActiveTeam(dungeonResults);
+            SaveAndShowExpericeEarnedByActiveTeam(dungeonResults);
         }
 
-        public void GiveEarnedExpToActiveTeam(DungeonScore dungeonResults)
+        public void SaveAndShowExpericeEarnedByActiveTeam(DungeonScore dungeonResults)
         {
             var totalExpGained = dungeonResults.MonstersSlain.Sum(m => m.ExpGivenOnDeath);
             var heroesOnTeam = new HeroesOnActiveTeamGetter().Get();
             var expPerHero = totalExpGained / heroesOnTeam.Count();
 
+            SaveEarnedExpForActiveTeam(heroesOnTeam, expPerHero);
+            DrawHeroes(heroesOnTeam, expPerHero);
+        }
+
+        public void SaveEarnedExpForActiveTeam(List<Hero> heroesOnTeam, double expPerHero)
+        {
             foreach (var hero in heroesOnTeam)
             {
                 hero.CurrentExp += expPerHero;
@@ -41,6 +49,25 @@ namespace PuzzleRpg
 
             var heroRepository = new HeroRepository();
             heroRepository.UpdateHeroes(heroesOnTeam);
+        }
+
+        public void DrawHeroes(List<Hero> heroesOnTeam, double expPerHero) {
+            var heroViewModels = HeroToViewModelMapper.GetHeroViewModels(heroesOnTeam);
+
+            //TODO: we always use 5 so it can be internal to this method.
+            var profileSize = ViewCalculations.GetHeroProfileSizeGiveNColumns(5);
+            var row = 0;
+            foreach (var hero in heroViewModels)
+            {
+                var heroProfile = new CustomControls.HeroProfileInHeroBox();
+                heroProfile.Draw(hero);
+                heroProfile.SetValue(Grid.RowProperty, row);
+                heroProfile.SetValue(Grid.ColumnProperty, 0);
+                heroProfile.Height = profileSize.Height;
+                heroProfile.Width = profileSize.Width;
+                ActiveTeamList.Children.Add(heroProfile);
+                row += 1;
+            }
         }
 
         public void OnScreenTap(object sender, GestureEventArgs e)
